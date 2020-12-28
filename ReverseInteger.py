f48d0b3309dd590216bfa830782a6e9bb54134d1
Ali H. Dulaimi <43238519+Decoder3-14@users.noreply.github.com>
2020-12-28 14:49:43 +0300

    Create ReverseInteger.py

---
+++ ReverseInteger.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverse(self, x):
+        if x < 0:
+            return -self.reverse(-x)
+        output = 0
+        while x:
+            pop = x % 10
+            x = x // 10
+            output = output * 10 + pop
+        if output > (2**31) - 1 or output < (-2**31):
+            return 0
+        return output if output <= 0x7fffffff else 0
