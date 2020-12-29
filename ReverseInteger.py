c306497083eccc0a227737cb40ca05de55f6e7c6
Ali H. Dulaimi <43238519+Decoder3-14@users.noreply.github.com>
2020-12-29 10:10:31 +0300

    Update ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -9,4 +9,4 @@ class Solution(object):
             output = output * 10 + pop
         if output > (2**31) - 1 or output < (-2**31):
             return 0
-        return output if output <= 0x7fffffff else 0
+        return output
